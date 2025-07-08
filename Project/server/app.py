46901817ce5981c012b7b036c0f9adab356b7c06
OmarVB77 <omar.vega.b@uni.pe>
2025-07-07 23:05:07 -0500

    Primer cambio

--- Project/server/app.py
+++ Project/server/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from src.Routes.route import modelRoute
 from pyngrok import ngrok
 ssl._create_default_https_context = ssl._create_unverified_context
-ngrok.set_auth_token('2hKn4UWWXYkrNTCBS8HWz0IKXtY_6YwXJMKDn4w3KWwTZUFfN')
+ngrok.set_auth_token('')
 app = Flask(__name__)
 modelRoute(app)
 CORS(app)
